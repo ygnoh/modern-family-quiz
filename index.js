8a3bda4d39fd56fd325df9d9bc2638dc18ab2566
yonggoonoh <nohyg90@gmail.com>
2020-03-19 23:21:24 +0900

    create quiz app

---
+++ index.js
@@ -0,0 +1,138 @@
+{
+    const source = `would you get them? : 애들 좀 데려다 줄래?
+You're not wearing that outfit. : 너 그 옷은 안 되겠다.
+Luke got his head stuck in the banister again : Luke가 또 머리를 난간에 끼었어요.
+It's on our bedside tab. : 침대 끄트머리에 있어.
+I was out of control growing up. : 자랄 때 난 통제 불능이었다.
+If Haley never wakes up on a beach in Florida, half-naked, I've done my job. : 만약 Haley가 반 나체로 Florida 비치에서 일어나지만 않는다면, 제 할 일을 다 한거죠.
+He tripped him. : 그가 발을 걸었어.
+Let's take it down a notch. : 좀 진정해.
+You gotta take that kid out. : 저 아이는 빼야죠.
+this must be your dad. : 이 분은 당신 아버지군요.
+Don't be fooled. : 속지 마세요.
+We just adopted her from Vietnam. : 우리 이 여자 아이를 베트남에서 막 입양했어요.
+we're bringing her home for the first time. : 우리는 그녀를 처음으로 집에 데려가는 중이에요.
+what are we talking about? : 우리 무슨 얘기 하는 중이야?
+we have been together for five years. : 우리는 함께 한 지 5년 되었다.
+we decided we really wanted to have a baby. : 우리가 아이를 정말 가지고 싶었다.
+we had initially asked one of our lesbian friends to be a surrogate. : 우리는 처음에 lesbian 친구 중 한 명에게 대리인이 되어 줄 수 있냐고 물었었다.
+they're already mean enough. : 그들은 이미 충분히 못됐다.
+Can you imagine one of them pregnant? : 그들 중 한 명이 임신한다는 걸 상상할 수 있어요?
+I'm gonna give the speech. : 한 마디 해야겠어.
+You're gonna be stuck with these people for the next five hours. : 이 사람들과 앞으로 5시간을 갇혀 있어야해.
+This baby would have grown up in a crowded orphanage if it wasn't for us. : 우리 아니었으면 이 아이는 붐비는 고아원에서 자랐을 거예요.
+Note to all of you : 모두들 잘 들으세요.
+shame on you. : 부끄러운 줄 아세요.
+Why do you keep getting stuck like this? : 왜 자꾸 이렇게 끼는 거야?
+I thought I could get out this time. : 이번엔 빠져나올 수 있을 거라 생각했어요.
+I'm just gonna say it. : 그냥 말해야겠어.
+I'm having a friend over today. : 오늘 친구 한 명 초대했어요.
+why don't you take it outside? : 밖으로 가져가는 게 어때?
+I'm kidding. : 장난이야.
+you guys are just going to embarrass me again. : 당신들 또 저를 창피하게 할 거잖아요.
+hang on a second. : 잠깐만.
+I'm bound to be a little surprised. : 약간 놀라기 마련이지.
+I had better charge the camcorder. : 난 캠코더 충전하는 게 좋겠다. (협박)
+That's my thing. : 그게 내 방식이죠. / 내 취향입니다.
+I didn't mean to. : 의도하지 않았어요.
+What did I tell you would happen if you got him a gun? : 그가 총을 가지면 무슨 일이 일어날 거라고 내가 말했었죠?
+Deal with this. : 이거 해결하세요.
+you have to follow through. : 완수해야 합니다. / 이행해야 합니다.
+You can shoot him afterwards. : 그 이후에 그를 쏠 수 있다.
+I guess that works for me. : 나도 그게 괜찮겠어.
+You would have stopped that goal if you weren't staring at that little girl. : 그 작은 소녀를 쳐다보고 있지 않았다면 그 골을 막을 수 있었을텐데.
+Why do you have to do things like that? : 왜 당신은 뭐든 꼭 그렇게 해야만 하는 거야?
+you're with me on this. : 너도 이거에 대해서 나랑 함께한다.
+I wanna go to the mall where she works. : 그녀가 일하는 mall에 가고 싶어요.
+first I need to get my white shirt, the silk one. : 우선 내 흰 셔츠를 가져 올 필요가 있어요. 실크로 된 거요.
+You hurt his feelings. : 너 그 애 상처줬어.
+One time, I'm not kidding you, we fell out the window together. : 어느 날은, 농담이 아니라, 우리는 함께 창문 밖으로 떨어졌어요.
+I'm hearing this for the first time. : 전 이 얘기 처음 듣습니다.
+This doesn't worry you? : 너는 이거 걱정 안 돼?
+What the hell is that supposed to mean? : 도대체 그게 무슨 뜻이야?
+I'm not saying anything. : 나는 아무 말 안하고 있다.
+I had Andre do it while we were gone. : 내가 Andre한테 우리 없는 동안 이거 하라고 시켰어.
+Even Pepper pointed it out on the way home from the airport. : 심지어 Pepper도 공항에서 집 오는 길에 그거 지적했어.
+I never told my family we were adopting a baby. : 나는 가족한테 우리가 아이 입양할 거라고 말한 적 없어.
+I don't blame you. : 너를 나무라지는 않을게
+I'm so relieved you understand. : 너가 이해해주니 너무 안심이 돼
+You're an avoider. : 너는 피하는 사람이잖아.
+you do have avoidance issues : 너는 매번 회피하는 문제가 있어.
+You have to scare him. : 너가 그를 무섭게 해야 한다.
+You two keep it real, know what I mean? : 너희 둘 진실되게 행동해, 무슨 말인지 알지?
+Not really. : 별로요.
+I slipped in the baby oil. : 베이비 오일에 미끄러졌어.
+I could defeat you if it came to a physical confrontation. : 만약 신체적 대립이 생긴다면 나 널 패배시킬 수 있다.
+I am on my side though. : 그렇지만 나 옆으로 누워있어.
+A poem I have written for her. : 그녀를 위해 내가 쓴 시
+I put my thoughts into words and now my words into action. : 내 생각을 단어로 옮겼고, 지금, 단어들을 행동으로 옮긴다.
+I'll give you 50 bucks not to do this. : 이거 하지 말라고 50 bucks 줄게.
+You ever see a bullfight? : 황소 싸움 본 적 있어?
+You're in such a bad mood. : 너 꽤 기분이 안 좋구나.
+You should only care what I think. : 너는 내가 무슨 생각을 하는지에 대해서만 신경 써야한다.
+So stop being a gloomy goose and stop being so hard on Manny. : 그러니까 우울한 거위 그만 되고, Manny한테 그렇게 딱딱하게 굴지마.
+I don't know what's gonna happen to him over there. : 저기에서 그에게 무슨 일이 일어날지 나는 모르겠다.
+That was pretty stupid of me : 그건 정말 멍청한 나였어요.
+We ask that all mall-walkers stay to the right. : 우리는 모든 mall-walkers들이 우측에 머무르길 부탁합니다.
+Alex, leave your sister alone. : Alex, 언니 좀 혼자 둬.
+What are they doing up there? : 걔네 위에서 뭐하고 있어?
+You wanna help me with the frosting? : frosting 하는 거 도와주길 원해?
+How many pairs of underwear do you have on? : 너 속옷 몇 벌이나 입은거야?
+Haley is not getting pregnant. : Haley는 임신하지 않을 거야.
+It's supposed to hurt. : 다치게 해야 해.
+The point is you're scared. : 중요한 건 너가 겁먹었다는 거야.
+we're just gonna go ahead and leave that open. : 우린 그냥 갈 거고, 그건 열려있게 둘 거야.
+You have got to talk to mom. : 너 엄마(mom)한테 말해야 한다.
+She is completely freaking out and embarrassing me! : 그녀는 완전히 흥분했고 날 창피주고 있어요.
+It's been five years. : 5년 되었다.
+Would you get in the spirit of things? : 기분 좀 맞춰줄래?
+I want you to just come straight out with it. : 난 너가 그냥 털어놨으면 좋겠다. 
+Nothing gay going on here. : 게이스러운 거 없어요.
+That's how she says "Phil" : 그게 그녀가 "Phil"을 말하는 방식이야.
+about that, I have something that I need to tell you guys. : 그거에 관해서, 나 여러분에게 말해야만 할 게 있어요.
+we didn't just go to Vietnam for pleasure. : 우리 베트남에 그저 놀러 간 게 아니에요.
+Haley had her first boy over today. : Haley가 그녀의 첫 남자를 오늘 초대 했다.
+What do you mean "bad idea"? : "bad idea"가 무슨 의미죠?
+Kids bring chaos and you don't handle it well. : 아이들은 혼란을 가져오는데 너는 그걸 잘 다룰 수 없다.
+Let's all chillax. : 모두들 진정해.
+Someone who's not insulting me notices he's not here. : 나를 모욕하고 있지 않은 누군가가 그가 여기 없다는 걸 알아 챘네요.
+You're better off because he was a bit of a drama queen. : 너 훨씬 더 나아. 왜냐면 그는 약간 드라마 퀸이었거든.
+say hi to Lily. : Lily에게 인사하세요
+Isn't that gonna be hard for her to say? : 그녀가 말하기에 어렵지 않을까?
+it's not like I wrote the book on fatherhood. : 내가 아버지 시절에 관한 책을 쓴 것도 아니잖아.
+I'm happy for you. : 잘 됐다.
+it's supposed to sound better in Spanish. : 스페인어에서 듣는 게 더 잘 들리도록 되어 있어.
+She's one of us now. : 이제 그녀는 우리 중 한 명이다.
+We're from different worlds, yet we somehow fit together. : 우린 서로 다른 세상에서 왔어요, 하지만, 우린 어떻게든 함께 맞춥니다.
+I stand before you now with only one agenda : 나는 지금 당신 앞에 오직 하나의 안건을 가지고 서있습니다.
+Luke, so far, hasn't beaten me in basketball. : Luke는 지금까지 농구에서 날 이긴 적이 없어요.`;
+
+    const storage = source.split("\n").reduce((acc, line) => {
+        const [en, ko] = line.split(":");
+
+        acc.push({en: en.trim(), ko: ko.trim()});
+
+        return acc;
+    }, []);
+    const len = storage.length;
+    let limit = 20;
+
+    while (limit--) {
+        const picked = storage[Math.random() * len | 0];
+        const question = picked.ko;
+        const answer = picked.en;
+        let response = window.prompt(`To English:\n\t${question}`);
+
+        while (response.trim() !== answer) {
+            if (response === "STOP") {
+                limit = 0;
+                break;
+            }
+
+            window.alert(`The answer is:\n\t${answer}\nTry again.`);
+            response = window.prompt(`To English:\n\t${question}`);
+        }
+    }
+
+    window.alert("You did well.");
+}
